9495ec9eb297d1e5e1e0c350c1d04f20af352654
blackhawk27 <65427186+blackhawk27@users.noreply.github.com>
2022-04-13 16:45:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -210,4 +210,6 @@ let addPostBtn = document.querySelector(".write-post-container");
 
 function addPost(){
     addPostBtn.classList.toggle("write-post-container-height");
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
